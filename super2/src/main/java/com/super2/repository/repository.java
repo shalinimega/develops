bbb3bd6c58a67822465376bf4fa04381caef698f
shalinimega <154418915+shalinimega@users.noreply.github.com>
2024-03-06 18:01:39 +0530

    Delete super2 directory

--- super2/src/main/java/com/super2/repository/repository.java
+++
@@ -1,9 +0,0 @@
-package com.super2.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.super2.entity.super2;
-
-public interface repository extends JpaRepository<super2, Integer> {
-
-}
