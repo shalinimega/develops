3c1a5181468f3f29095f86b15df7525e638124cf
shalinimega <154418915+shalinimega@users.noreply.github.com>
2024-03-06 18:01:57 +0530

    Delete super1 directory

--- super1/src/main/java/com/super1/repository/repository.java
+++
@@ -1,9 +0,0 @@
-package com.super1.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.super1.entitiy.super1;
-
-public interface repository  extends JpaRepository<super1, Integer>{
-
-}
