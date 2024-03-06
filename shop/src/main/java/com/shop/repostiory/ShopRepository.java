3ea652d09d918644edcfb9085d4941785d8910d6
shalinimega <154418915+shalinimega@users.noreply.github.com>
2024-03-06 18:02:32 +0530

    Delete shop directory

--- shop/src/main/java/com/shop/repostiory/ShopRepository.java
+++
@@ -1,9 +0,0 @@
-package com.shop.repostiory;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.shop.entity.shop;
-
-public interface ShopRepository  extends JpaRepository<shop, Integer>{
-
-}
