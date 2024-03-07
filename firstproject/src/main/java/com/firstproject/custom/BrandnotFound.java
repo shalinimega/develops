d293aa00125bf9822ad32b3c796029dad89c39c6
shalinimega <154418915+shalinimega@users.noreply.github.com>
2024-03-07 09:22:02 +0530

    Delete firstproject directory

--- firstproject/src/main/java/com/firstproject/custom/BrandnotFound.java
+++
@@ -1,9 +0,0 @@
-package com.firstproject.custom;
-
-public class BrandnotFound  extends Exception{
-	public BrandnotFound(String a)
-	{
-		super(a);
-	}
-
-}
--- firstproject/src/main/java/com/firstproject/custom/Priceless.java
+++
@@ -1,9 +0,0 @@
-package com.firstproject.custom;
-
-public class Priceless  extends Exception{
-	public Priceless(String a)
-	{
-		super(a);
-	}
-
-}
