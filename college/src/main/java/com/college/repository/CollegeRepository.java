2852c295fdc5d13cd6ab1d1ac706974916b6de1f
shalinimega <154418915+shalinimega@users.noreply.github.com>
2024-03-07 09:22:34 +0530

    Delete college directory

--- college/src/main/java/com/college/repository/CollegeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.college.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.college.entity.College;
-
-public interface CollegeRepository extends JpaRepository<College,Integer>{
-
-}
