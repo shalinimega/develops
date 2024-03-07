8086ced4563a73acd71de42597446d45c2ab2d06
shalinimega <154418915+shalinimega@users.noreply.github.com>
2024-03-07 09:21:40 +0530

    Delete StudentMarkSheet directory

--- StudentMarkSheet/src/main/java/com/StudentMarkSheet/repository/MarkSheetRepository.java
+++
@@ -1,9 +0,0 @@
-package com.StudentMarkSheet.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.StudentMarkSheet.entity.MarkSheet;
-
-public interface MarkSheetRepository extends JpaRepository<MarkSheet, Integer> {
-
-}
